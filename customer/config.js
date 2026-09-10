a972e96ee576d962fb0c8f511f46e49ea5710546
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-10 13:38:31 +0330

    Update config.js

--- customer/config.js
+++ customer/config.js
@@ -26,22 +26,5 @@ export const CONFIG = {
     'https://nshn.ir/357b1MaUNJjDV8',
 
   googleMapsUrl:
-    'https://maps.app.goo.gl/CpzYyvSusBmFViwi7',
-
-  faq: [
-    {
-      q:
-        '📝 راهنمای ثبت سفارش',
-
-      a:
-        `برای ثبت سفارش یا دریافت اطلاعات درباره محصولات، از طریق آیدی پشتیبانی با ما در ارتباط باشید.\n\n` +
-        `📞 آیدی پشتیبانی و ثبت سفارش:\n` +
-        `@shoma_shop_sup\n\n` +
-        `📱 آیدی رسمی ما در تلگرام، ایتا و روبیکا:\n` +
-        `@shoma_shop_sup\n\n` +
-        `⚠️ توجه:\n` +
-        `تنها آیدی رسمی پشتیبانی و ثبت سفارش ما همین آیدی است:\n` +
-        `@shoma_shop_sup`
-    }
-  ]
+    'https://maps.app.goo.gl/CpzYyvSusBmFViwi7'
 };

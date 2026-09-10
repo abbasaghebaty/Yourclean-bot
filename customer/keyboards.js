35b178bf72d0274580c714a3e1c844ae05a012d0
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-10 18:15:09 +0330

    Update keyboards.js

--- customer/keyboards.js
+++ customer/keyboards.js
@@ -33,7 +33,7 @@ export function mainReplyKeyboard() {
       ],
       [
         {
-          text: '⚠️ صندوق انتقادات و پیشنهادات',
+          text: '📫 صندوق انتقادات و پیشنهادات',
           style: 'danger'
         }
       ]
